2cadb34d2748a02ee7c487b339740f98f7fe8a94
lisongling <lisongl123@163.com>
2018-09-02 09:58:03 +0800

    创建文档结构

---
+++ src/main/java/com/example/demo/config/dao/SessionFactoryConfiguration.java
@@ -0,0 +1,47 @@
+package com.example.demo.config.dao;
+
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+
+/**
+ * Created by lsl on 2018/9/2.
+ */
+@Configuration
+public class SessionFactoryConfiguration {
+
+    @Value("${mybatis_config_file}")
+    private String mybatisConfigFilePath;
+
+    @Value("${mapper_path}")
+    private String mapperPath;
+
+    @Value("${entity_package}")
+    private String entityPackage;
+
+    @Autowired
+    @Qualifier("dataSource")
+    private DataSource dataSource;
+
+    @Bean(name="sqlSerrionFactory")
+    public SqlSessionFactoryBean createsqlSessionFactoryBean() throws IOException {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setConfigLocation(new ClassPathResource(mybatisConfigFilePath ));
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+
+        String packageSearchPath = PathMatchingResourcePatternResolver.CLASSPATH_ALL_URL_PREFIX + mapperPath;
+        sqlSessionFactoryBean.setMapperLocations(resolver.getResources(packageSearchPath));
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        sqlSessionFactoryBean.setTypeAliasesPackage(entityPackage);
+
+        return sqlSessionFactoryBean;
+    }
+}
